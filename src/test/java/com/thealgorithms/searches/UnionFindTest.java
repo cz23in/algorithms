2b06f4043370ff777fa9b1e55df55761c4f412c5
cz23in <cz23in@aol.com>
2024-06-24 20:42:30 +0200

    Added improved test case for UnionFind for adam

--- src/test/java/com/thealgorithms/searches/UnionFindTest.java
+++ src/test/java/com/thealgorithms/searches/UnionFindTest.java
@@ -1,5 +1,6 @@
 package com.thealgorithms.searches;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UnionFindTest {
@@ -7,7 +8,18 @@ public class UnionFindTest {
     @Test
     public void unionFindTest() {
         UnionFind.coverage.reset();
+        UnionFind uf = new UnionFind(5);
 
+        Assertions.assertEquals(
+                uf.toString(),
+                "p [0, 1, 2, 3, 4] r [0, 0, 0, 0, 0]\n"
+        );
+        
+        uf.union(1, 2);
+        Assertions.assertEquals(
+                uf.toString(),
+                "p [0, 1, 1, 3, 4] r [0, 1, 0, 0, 0]\n"
+        );
         UnionFind.coverage.print();
     }
 }
