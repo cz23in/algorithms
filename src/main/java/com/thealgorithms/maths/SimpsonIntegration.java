3899b06af695f2a5615b1ff346601e02752af194
XqZhangz <x16.zhang@student.vu.nl>
2024-06-25 00:00:44 +0200

    back

--- src/main/java/com/thealgorithms/maths/SimpsonIntegration.java
+++ src/main/java/com/thealgorithms/maths/SimpsonIntegration.java
@@ -1,28 +1,9 @@
 package com.thealgorithms.maths;
 
 import java.util.TreeMap;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SimpsonIntegration {
 
-    private static final Map<String, Boolean> coverageMap = new HashMap<>();
-
-    private static void initializeCoverageMap() {
-        coverageMap.put("forloop.1", false);
-        coverageMap.put("forloop.2", false);
-        coverageMap.put("if.1", false);
-        coverageMap.put("elif.1", false);
-        coverageMap.put("else.1", false);
-    }
-
-    public static Map<String, Boolean> getCoverageMap() {
-        return new HashMap<>(coverageMap);
-    }
-
-    public static void resetCoverageMap() {
-        initializeCoverageMap();
-    }
 
     /*
      * @param N: Number of intervals (must be even number N=2*k)
@@ -41,7 +22,6 @@ public class SimpsonIntegration {
 
         // Create the table of xi and yi points
         for (int i = 0; i <= n; i++) {
-            coverageMap.put("forloop.1", true);
             temp = f(xi); // Get the value of the function at that point
             data.put(i, temp);
             xi += h; // Increase the xi to the next point
@@ -50,17 +30,14 @@ public class SimpsonIntegration {
         // Apply the formula
         double integralEvaluation = 0;
         for (int i = 0; i < data.size(); i++) {
-            coverageMap.put("forloop.2", true);
+
             if (i == 0 || i == data.size() - 1) {
-                coverageMap.put("if.1", true);
                 integralEvaluation += data.get(i);
                 System.out.println("Multiply f(x" + i + ") by 1");
             } else if (i % 2 == 1) {
-                coverageMap.put("elif.1", true);
                 integralEvaluation += (double) 4 * data.get(i);
                 System.out.println("Multiply f(x" + i + ") by 4");
             } else {
-                coverageMap.put("else.1", true);
                 integralEvaluation += (double) 2 * data.get(i);
                 System.out.println("Multiply f(x" + i + ") by 2");
             }
