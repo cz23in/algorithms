6699363f58cc2be2a44d61c439f9e2f0110f6139
cz23in <cz23in@aol.com>
2024-06-24 20:29:38 +0200

    Added improved test for adam second function and coverage screenshots

--- src/test/java/com/thealgorithms/stacks/NextSmallerElementTest.java
+++ src/test/java/com/thealgorithms/stacks/NextSmallerElementTest.java
@@ -1,5 +1,6 @@
 package com.thealgorithms.stacks;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NextSmallerElementTest {
@@ -7,7 +8,10 @@ public class NextSmallerElementTest {
     @Test
     public void nextSmallerElementTest() {
         NextSmallerElement.coverage.reset();
-
+        int[] input1 = {2, 7, 3, 5, 4, 6, 8};
+        int[] result1 = NextSmallerElement.findNextSmallerElements(input1);
+        int[] expected1 = {-1, 2, 2, 3, 3, 4, 6};
+        Assertions.assertArrayEquals(expected1, result1);
         NextSmallerElement.coverage.print();
     }
 
